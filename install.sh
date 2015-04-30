1e0a56a35830cdd65d370a3205729fa26125ef9d
nelson kigen <nellyk89@gmail.com>
2015-04-30 14:34:42 +0200

    Minor modifications to install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,11 @@
 
 INSTALL_PREFIX='/usr/local/bin'
 TO_INSTALL='scripts/*'
+TMP_DIR="/tmp/zeinarmy"
+DEPS=(
+'git'
+'aria2c'
+)
 
 check_version(){
 	.
@@ -17,13 +22,33 @@ prepend(){
 	echo "$1" | cat - $2 > .zatemp && mv .zatemp $2
 }
 
+install_zeinarmy(){
+	echo "installing zeinarmy to "${INSTALL_PREFIX}
+	sudo cp -r $TO_INSTALL $INSTALL_PREFIX
+	sudo mkdir -p /etc/zeinarmy
+	#prepend "CONFIG_DIR=/etc/zeinarmy" $(readlink -f config)
+	sudo cp -r config /etc/zeinarmy
+}
+
+#Currently only deps for debian and ubuntu based systems are supported
+install_deps(){
+	source /etc/lsb-release
+	sudo add-apt-repository "deb http://some-repo/ubuntu $DISTRIB_CODENAME main"
+	if [ "r"$DISTRIB_CODENAME = "r" ]; then
+		echo "Deps could not be installed" && exit 1
+	elif [ $DISTRIB_CODENAME = "debian" ]; then
+		
+	fi
+
+}
 main(){
 	if [ "r""$1" = "r" ]; then
-		echo "installing zeinarmy to "${INSTALL_PREFIX}
-		sudo cp -r $TO_INSTALL $INSTALL_PREFIX
-		sudo mkdir -p /etc/zeinarmy
-		#prepend "CONFIG_DIR=/etc/zeinarmy" $(readlink -f config)
-		sudo cp -r config /etc/zeinarmy
+		if [ "r""$1" = "r" ]; then
+			install_zeinarmy
+		else
+			install_deps
+			install_zeinarmy
+		fi
 	else
 		echo "installing zeinarmy to ""$1"
 		mkdir -p $1
